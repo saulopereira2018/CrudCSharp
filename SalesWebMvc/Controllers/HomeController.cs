e0c4d2683093dc818117814989a2a9ca9b0e6003
Saulo Pereira <saulopsg@hotmail.com>
2022-07-27 17:05:15 -0400

    Project Created

---
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models.ViewModels;
+
+namespace SalesWebMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Sobre()
+        {
+            ViewData["Message"] = "CRUD C# - C-Sharp";
+            ViewData["Professor"] = "Saulo Pereira";
+
+            return View();
+        }
+
+        public IActionResult Contato()
+        {
+            ViewData["Message"] = "Página de contato";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
